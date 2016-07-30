9f78f9c7ced31219d80d425b9af9ad2defc959bc
Mohanapriyan <mohanapriyans3@gmail.com>
2016-07-30 18:08:44 +0530

    Create Stringtoint.java

---
+++ Stringtoint.java
@@ -0,0 +1,11 @@
+package simplecodes;
+
+public class Stringtoint {
+
+	public static void main(String[] args) {
+		String str="1234";
+	     int a=Integer.parseInt(str);
+	     System.out.println(a);
+	}
+
+}
