95714f94de49d830b6de8c13d5f0599e85bbd5cd
Mohanapriyan <mohanapriyans3@gmail.com>
2016-07-31 12:28:44 +0530

    Create Leapornot.java

---
+++ Leapornot.java
@@ -0,0 +1,18 @@
+package simplecodes;
+
+public class Leapornot {
+
+	
+	public static void main(String[] args) {
+		int year=2000;
+		if(year%4==0)
+		{
+			System.out.println(year+"\t"+"is leap year");
+		}
+		else
+		{
+			System.out.println(year+"\n"+"is not a leap year");
+		}
+	}
+
+}
