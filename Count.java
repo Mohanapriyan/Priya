aff2fefe9da059717a46301d7c4eb9af0516037d
Mohanapriyan <mohanapriyans3@gmail.com>
2016-08-04 15:09:13 +0530

    Create Count.java

---
+++ Count.java
@@ -0,0 +1,12 @@
+package simplecodes;
+
+public class Count {
+
+	
+	public static void main(String[] args) {
+		int number=12345678;
+		int count=Integer.toString(number).length();
+		System.out.println("the number of the digits in the integer is:"+count);
+	}
+
+}
