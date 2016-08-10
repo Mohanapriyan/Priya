9490eb1d3cf5ff0be9280c2427c65295249e2d5f
Mohanapriyan <mohanapriyans3@gmail.com>
2016-08-10 17:19:27 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,14 @@
+package number;
+class Sum
+{
+   public static void main(String []args)
+   {
+      int sum=0;
+      int limit=10;
+      for(int i=0;i<10;i++)
+      {
+          sum+=i;
+      }
+      System.out.println(sum);
+   }
+}
