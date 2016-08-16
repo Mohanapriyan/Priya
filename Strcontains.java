efd1677683e02bdf6af25df8dbc4ea51ecbaf2bc
Mohanapriyan <mohanapriyans3@gmail.com>
2016-08-16 17:25:13 +0530

    Create Strcontains.java

---
+++ Strcontains.java
@@ -0,0 +1,16 @@
+package simplecodes;
+
+public class Strcontains {
+
+	public static void main(String[] args) {
+	String str="welcome";
+	if(str.contains("m"))
+	{
+		System.out.println("the string contains the character");
+	}
+	else
+	{
+		System.out.println("the string is not contain with the character");
+	}
+   }
+}
