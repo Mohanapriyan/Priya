2ccbbb6ceaacf5d889ea734831290bb7b7508a78
Mohanapriyan <mohanapriyans3@gmail.com>
2016-08-18 17:10:21 +0530

    Create Rstring.java

---
+++ Rstring.java
@@ -0,0 +1,9 @@
+package logical;
+public class Rstring {
+
+	public static void main(String[] args) {
+    StringBuffer str=new StringBuffer("hello");
+    System.out.println(str.reverse());
+	}
+
+}
