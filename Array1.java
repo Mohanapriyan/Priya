9c8b3654c79f19a77d175576e522e6c4639c528c
Mohanapriyan <mohanapriyans3@gmail.com>
2016-08-18 16:48:17 +0530

    Create Array1.java

---
+++ Array1.java
@@ -0,0 +1,13 @@
+public class Array1 {
+
+	
+	public static void main(String[] args) {
+	int n[]={1,2,3,4,5,6,7,8,9};
+	int t[]={n[2],n[1],n[0],n[5],n[4],n[3],n[8],n[7],n[6]};
+	for(int i=0;i<n.length;i++)
+	{
+	System.out.print(t[i]+",");
+	}
+}
+
+}
